916777ececdb534d85fdbd6a97a6e5c63b319fe6
Elanny Torres <torres.elanny@gmail.com>
2021-12-19 11:51:25 -0500

    Arreglo de errores en el timeline

--- src/firebase/timeline.js
+++ src/firebase/timeline.js
@@ -8,31 +8,62 @@ import { app } from './app.js';
 
 const db = getFirestore(app);
 
-window.onload = function () {
-  const postCreater = document.querySelector(
-    '#root div section.postContainer form#postForm'
-  );
-  console.log(postCreater);
+export const savePost = async () => {
+  const postCreater = document.querySelector('#postForm');
   const postContainer = document.querySelector('.posted');
-  console.log(postContainer);
 
   postCreater.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const postText = postCreater['textDescription'];
-    const newPost = await addDoc(collection(db, 'posts'), {
-      description: `${postText.value}`,
+    const postText = postCreater.textDescription;
+    if (postText.value === '') {
+      /* console.log('input vacío'); */
+      alert('Mensaje vacío.\n Escriba algo para poder compartir.');
+    } else {
+      const newPost = await addDoc(collection(db, 'posts'), {
+        description: `${postText.value}`,
+      });
+      console.log('Document written with ID: ', newPost.id);
+      console.log(postText.value);
+
+      postCreater.reset();
+      postText.focus();
+
+      postContainer.innerHTML = '';
+
+      const querySnapshot = await getDocs(collection(db, 'posts'));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        postContainer.innerHTML += `
+        <div class="postedOne">
+          <div class="postedOneTitle">Publicado por ${doc.id}</div>
+          <div class="postedText">${doc.data().description}</div>
+          <div class="postedBtns">
+            <button class="likePosted">Like</button>
+            <button class="sharePosted">Share</button>
+            <button class="editPosted">Edit</button>
+            <button class="deletePosted">Delete</button>
+          </div>
+        </div>
+      `;
+      });
+    }
+  });
+
+  /*
+  for (var i = 0; i < btnsDelete.length; i++) {
+    btnsDelete[i].addEventListener('click', async () => {
+      console.log('deleting');
     });
-    console.log('Document written with ID: ', newPost.id);
-    console.log(postText.value);
+  } */
+};
 
-    postCreater.reset();
-    postText.focus();
-
-    const querySnapshot = await getDocs(collection(db, 'posts'));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      postContainer.innerHTML += `
+export const loadPosts = async () => {
+  const postContainer = document.querySelector('.posted');
+  const querySnapshot = await getDocs(collection(db, 'posts'));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    postContainer.innerHTML += `
       <div class="postedOne">
         <div class="postedOneTitle">Publicado por ${doc.id}</div>
         <div class="postedText">${doc.data().description}</div>
@@ -40,9 +71,19 @@ window.onload = function () {
           <button class="likePosted">Like</button>
           <button class="sharePosted">Share</button>
           <button class="editPosted">Edit</button>
+          <button class="deletePosted">Delete</button>
         </div>
       </div>
     `;
-    });
   });
+};
+
+export const deleteBtn = async () => {
+  const btnsDelete = document.querySelectorAll('.deletePosted');
+  console.log(btnsDelete);
+  /* btnsDelete.addEventListener('click', () => {
+    console.log('deleting');
+  });
+  #root div section.postContainer div.posted div.postedOne button
+  */
 };

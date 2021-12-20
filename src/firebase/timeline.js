1d90096bc7e6264bb08f94be06c6c63119ac1bad
Elanny Torres <torres.elanny@gmail.com>
2021-12-20 10:03:00 -0500

    Delete posts

--- src/firebase/timeline.js
+++ src/firebase/timeline.js
@@ -3,10 +3,51 @@ import {
   collection,
   addDoc,
   getDocs,
+  doc,
+  deleteDoc,
 } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js';
 import { app } from './app.js';
 
 const db = getFirestore(app);
+
+export const deleteBtn = async () => {
+  const btnsDelete = document.querySelectorAll('.deletePosted');
+  const postContainer = document.querySelector('.posted');
+
+  btnsDelete.forEach((btn) => {
+    btn.addEventListener('click', async () => {
+      console.log('deleting', btn.id);
+      const deleted = await deleteDoc(doc(db, 'posts', btn.id));
+
+      postContainer.innerHTML = '';
+
+      loadPosts();
+      deleteBtn();
+    });
+  });
+};
+
+export const loadPosts = async () => {
+  const postContainer = document.querySelector('.posted');
+  const querySnapshot = await getDocs(collection(db, 'posts'));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    postContainer.innerHTML += `
+      <div class="postedOne">
+        <div class="postedOneTitle">Publicado por ${doc.id}</div>
+        <div class="postedText">${doc.data().description}</div>
+        <div class="postedBtns">
+          <button class="likePosted"><i class="fas fa-heart"></i></button>
+          <button class="editPosted"><i class="fas fa-pencil-alt"></i></button>
+          <button class="deletePosted" id="${doc.id}">
+          <i class="far fa-trash-alt"></i>
+          </button>
+        </div>
+      </div>
+    `;
+  });
+  deleteBtn();
+};
 
 export const savePost = async () => {
   const postCreater = document.querySelector('#postForm');
@@ -31,59 +72,8 @@ export const savePost = async () => {
 
       postContainer.innerHTML = '';
 
-      const querySnapshot = await getDocs(collection(db, 'posts'));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        postContainer.innerHTML += `
-        <div class="postedOne">
-          <div class="postedOneTitle">Publicado por ${doc.id}</div>
-          <div class="postedText">${doc.data().description}</div>
-          <div class="postedBtns">
-            <button class="likePosted">Like</button>
-            <button class="sharePosted">Share</button>
-            <button class="editPosted">Edit</button>
-            <button class="deletePosted">Delete</button>
-          </div>
-        </div>
-      `;
-      });
+      loadPosts();
+      deleteBtn();
     }
   });
-
-  /*
-  for (var i = 0; i < btnsDelete.length; i++) {
-    btnsDelete[i].addEventListener('click', async () => {
-      console.log('deleting');
-    });
-  } */
-};
-
-export const loadPosts = async () => {
-  const postContainer = document.querySelector('.posted');
-  const querySnapshot = await getDocs(collection(db, 'posts'));
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
-    postContainer.innerHTML += `
-      <div class="postedOne">
-        <div class="postedOneTitle">Publicado por ${doc.id}</div>
-        <div class="postedText">${doc.data().description}</div>
-        <div class="postedBtns">
-          <button class="likePosted">Like</button>
-          <button class="sharePosted">Share</button>
-          <button class="editPosted">Edit</button>
-          <button class="deletePosted">Delete</button>
-        </div>
-      </div>
-    `;
-  });
-};
-
-export const deleteBtn = async () => {
-  const btnsDelete = document.querySelectorAll('.deletePosted');
-  console.log(btnsDelete);
-  /* btnsDelete.addEventListener('click', () => {
-    console.log('deleting');
-  });
-  #root div section.postContainer div.posted div.postedOne button
-  */
 };

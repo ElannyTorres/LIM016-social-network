52343434d6ba6222443180a9c0fafd3c1d44d37c
Brisset <corcinobrisset@gmail.com>
2021-12-21 08:29:55 -0500

    ajuste de los cambios nuevos index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,6 @@
 import { Home, login, loginAuthGoogle } from './home.js';
 import { singUp, registrar, authGoogle } from './singUp.js';
-<<<<<<< HEAD
-=======
 import { savePost, deleteBtn, loadPosts } from '../firebase/timeline.js';
->>>>>>> f698e943281b72b27166dd8fc746eb332e351275
 import Posts from './posts.js';
 import Profile from './profile.js';
 import Default from './default.js';
@@ -22,16 +19,11 @@ const components = {
     deleteBtn,
   },
   default: Default,
-<<<<<<< HEAD
-  singUp: { singUp, registrar, authGoogle },
-
-=======
   singUp: {
     singUp,
     registrar,
     authGoogle,
   },
->>>>>>> f698e943281b72b27166dd8fc746eb332e351275
 };
 
 export { components };

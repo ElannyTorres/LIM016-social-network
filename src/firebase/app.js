921580b5ce320bb4f52bc1365a2ba66c5e18e2ba
Elanny Torres <torres.elanny@gmail.com>
2022-01-10 10:15:58 -0500

    funcionalidad para editar

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,6 +1,27 @@
 // eslint-disable-next-line import/no-unresolved
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
 
+/* Inicializanco FireStore */
+import {
+  getFirestore,
+  // addDoc,
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  // query,
+  // where,
+  getDocs,
+  // getDatabase,
+  // ref,
+  // child,
+  // get,
+  // onValue,
+  getDoc,
+  updateDoc,
+  // eslint-disable-next-line import/no-unresolved
+} from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js';
+
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAnNJ2wh2QwS1SlbaDA1dpeWs_c_xE8_4o',
@@ -12,3 +33,23 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export {
+  getFirestore,
+  // addDoc,
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  // query,
+  // where,
+  getDocs,
+  // getDatabase,
+  // ref,
+  // child,
+  // get,
+  // onValue,
+  getDoc,
+  updateDoc,
+};
